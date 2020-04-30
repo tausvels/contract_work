6cec4ef75d13efab7caa5a3af8d9194301089d1b
tausvels <engr.tausifkhan@gmail.com>
2020-04-29 23:17:14 -0700

    added the react boiler placte

--- customQuiz/frontend/src/App.js
+++ customQuiz/frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import About from './screens/about/About';
 // ---- import components for ROUTING ----------------------------------------- //
 import Home from './screens/home/Home';
-import About from './screens/about/About';
+
 
 function App() {
   // state is the overall STATE object that has sub states and can be found in the index.js of the reducer folder
