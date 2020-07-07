3b0ea7d0e3b394864498ed59dc7f32f98dc384bf
tausvels <engr.tausifkhan@gmail.com>
2020-07-07 03:04:57 -0700

    implemented the contact component

--- ecomm_prototype/src/screens/contact/ContactCard.jsx
+++ ecomm_prototype/src/screens/contact/ContactCard.jsx
@@ -4,7 +4,10 @@ const sms = require('../../assets/sms.PNG');
 function ContactCard(props) {
   return (
     <div className="contact-card">
-      <img src={sms}/>
+      {/* <img src={sms}/> */}
+      <div className="white-background"></div>
+      <div className="blue-background"></div>
+      <img style={{zIndex: 3}} className='contact-card-img'src='https://www.entripy.com/Themes/Entripy/Content/17/images/blocks-ico7.svg' />
       <p>Live Chat</p>
     </div>
   );
